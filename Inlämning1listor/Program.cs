5171a8262d9c2d677eca8421c63b21326dd3b155
majo06002 <Max-Lukas@outlook.com>
2024-02-02 12:26:40 +0100

    Add project files.

---
+++ Inlämning1listor/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+namespace inlämning1listor
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Här får du skriva in valfritt antal namn, avsluta med tom rad");
+
+            List<string> lista = new List<string>();
+
+            string nyttNamn = " ";
+            while (nyttNamn != "")
+            {
+                Console.WriteLine("Skriv in ett namn");
+                nyttNamn = Console.ReadLine();
+                if (nyttNamn != "")
+                {
+                    lista.Add(nyttNamn);
+                }
+            }
+
+            lista.Sort();
+
+            Console.WriteLine("Här är namnen i omvänd bokstavsordning");
+            for (int i = lista.Count - 1; i >= 0; i--)
+            {
+                Console.WriteLine(lista[i]);
+            }
+
+        }
+    }
+}
